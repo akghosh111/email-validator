37e8ae585a25a650eef1e2c4b1792166f467675b
akghosh111 <ghoshkiran2017@gmail.com>
2024-04-05 21:23:41 +0530

    displaying results by updating in the dom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-console.log("chup bsdk")
+console.log("chup bsdk");
+
+document.addEventListener("DOMContentLoaded", function() {
+    let result = {
+        "tag": "",
+        "free": true,
+        "role": false,
+        "user": "anukiranghosh",
+        "email": "anukiranghosh@gmail.com",
+        "score": 0.64,
+        "state": "deliverable",
+        "domain": "gmail.com",
+        "reason": "valid_mailbox",
+        "mx_found": true,
+        "catch_all": null,
+        "disposable": false,
+        "smtp_check": true,
+        "did_you_mean": "",
+        "format_valid": true
+    };
+
+    let str = '';
+    for (key of Object.keys(result)) {
+        str = str + `<div>${key}: ${result[key]}</div>`;
+    }
+    console.log(str);
+    let resultCont = document.getElementById("resultCont");
+    if (resultCont) {
+        resultCont.innerHTML = str;
+    } else {
+        console.error("resultCont element not found!");
+    }
+});
